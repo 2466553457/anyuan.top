0f95cad96e19916e60126d807f1bb47f16876a8a
2466553457 <2995209639@qq.com>
2024-02-28 17:26:12 +0800

    Add files via upload

--- zhuluoji1/script.js
+++ zhuluoji1/script.js
@@ -23,7 +23,7 @@ passwordForm.addEventListener("submit", function (event) {
     passwordPage.classList.add("hidden");
 
     // 自动跳转到下一个页面
-    window.location.href = "zhuluoji.html";
+    window.location.href = "zhuluoji5147.html";
   } else {
     // 如果密码错误，则弹出警告框
     alert("密码错误，请重新输入！");
