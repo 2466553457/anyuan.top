c8b9d6ab4214a087429ed27a58b65049c53a5a52
2466553457 <2995209639@qq.com>
2024-03-06 22:08:54 +0800

    Add files via upload

--- dipingxian5/script.js
+++ dipingxian5/script.js
@@ -6,7 +6,7 @@ const passwordInput = document.getElementById("password-input");
 const goToNextPageButton = document.getElementById("go-to-next-page-button");
 
 // 设置密码
-const password = "ay5181";
+const password = "ay6865";
 
 // 监听密码输入表单的提交事件
 passwordForm.addEventListener("submit", function (event) {
@@ -23,7 +23,7 @@ passwordForm.addEventListener("submit", function (event) {
     passwordPage.classList.add("hidden");
 
     // 自动跳转到下一个页面
-    window.location.href = "dipingxian1779.html";
+    window.location.href = "aedfh1734.html";
   } else {
     // 如果密码错误，则弹出警告框
     alert("密码错误，请重新输入！");
