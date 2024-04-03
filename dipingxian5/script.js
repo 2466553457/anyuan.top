54999715cf7f49a5c22f674d01ffe52865b170cb
2466553457 <2995209639@qq.com>
2024-04-03 22:13:20 +0800

    Add files via upload

--- dipingxian5/script.js
+++ dipingxian5/script.js
@@ -6,7 +6,7 @@ const passwordInput = document.getElementById("password-input");
 const goToNextPageButton = document.getElementById("go-to-next-page-button");
 
 // 设置密码
-const password = "ay6865";
+const password = "ay5082";
 
 // 监听密码输入表单的提交事件
 passwordForm.addEventListener("submit", function (event) {
@@ -23,9 +23,23 @@ passwordForm.addEventListener("submit", function (event) {
     passwordPage.classList.add("hidden");
 
     // 自动跳转到下一个页面
-    window.location.href = "aedfh1734.html";
+    window.location.href = "dipingxian1779.html";
   } else {
     // 如果密码错误，则弹出警告框
     alert("密码错误，请重新输入！");
   }
 });
+
+// // 检查localStorage中的标志
+// var hasRedirected = localStorage.getItem("hasRedirected");
+
+// // 如果localStorage中的标志不存在或者为false，则进行跳转
+// if (!hasRedirected) {
+//   // 设置localStorage标志为true
+//   localStorage.setItem("hasRedirected", true);
+// } else {
+//   // 如果已经跳转过了，则直接跳转到下一个页面
+//   window.location.href = "dipingxian1779.html";
+// }
+// // 清除localStorage中的数据,最好不要使用，使用后悔，责任一概不担！
+// // localStorage.clear();
