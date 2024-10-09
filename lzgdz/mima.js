d20d0f74df68e14ee3ca6458999ef0db6d42b80a
2466553457 <2995209639@qq.com>
2024-10-09 19:22:40 +0800

    Add files via upload

---
+++ lzgdz/mima.js
@@ -0,0 +1,26 @@
+function validateForm() {
+  var captcha = document.getElementById("captcha").value;
+  var generatedCaptcha = document.getElementById("generatedCaptcha").innerText;
+
+  if (captcha === generatedCaptcha) {
+    // 跳转到另一个页面
+    window.location.href = "lzgdz2530.html";
+  } else {
+    alert("验证码错误，请重新输入！");
+  }
+
+  return false; // 防止表单提交
+}
+
+function generateCaptcha() {
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var captcha = "";
+
+  for (var i = 0; i < 4; i++) {
+    captcha += `<span class="captcha-char">${chars.charAt(
+      Math.floor(Math.random() * chars.length)
+    )}</span>`;
+  }
+
+  document.getElementById("generatedCaptcha").innerHTML = captcha;
+}
